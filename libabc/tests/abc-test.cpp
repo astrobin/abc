472d4f5fcc9fada81b7536a585459f7196fa1586
Alberto Mardegan <mardy@users.sourceforge.net>
2012-09-23 11:13:26 +0300

    Tests: add AbcTest::imageSetBounds()

--- libabc/tests/abc-test.cpp
+++ libabc/tests/abc-test.cpp
@@ -22,7 +22,9 @@
 
 #include "image-set.h"
 #include "image.h"
+
 #include <QDebug>
+#include <QRect>
 
 #define UTF8(s) QString::fromUtf8(s)
 
@@ -53,9 +55,9 @@ void AbcTest::imageSetAverage()
     QVERIFY(ok);
 
     ImageSet images;
-    images.addImage(source);
-    images.addImage(source);
-    images.addImage(source);
+    QVERIFY(images.addImage(source));
+    QVERIFY(images.addImage(source));
+    QVERIFY(images.addImage(source));
 
     Image average = images.average();
     QCOMPARE(average.size(), source.size());
@@ -64,6 +66,36 @@ void AbcTest::imageSetAverage()
     for (long i = 0; i < numPixels; i++) {
         QVERIFY(qFuzzyCompare(average.d->pixels[i], source.d->pixels[i]));
     }
+}
+
+void AbcTest::imageSetBounds()
+{
+    Image source;
+
+    bool ok = source.load("UIT.fits");
+    QVERIFY(ok);
+
+    ImageSet images;
+    QVERIFY(images.addImage(source, QTransform()));
+
+    QRect imageRect = QRect(QPoint(0, 0), source.size());
+    QRect expectedBounds = imageRect;
+    QCOMPARE(images.boundingRect(), expectedBounds);
+
+    /* We cannot add an image without an associated transformation, after
+     * adding a transformed one. */
+    QVERIFY(!images.addImage(source));
+
+    // Add a translated image
+    QVERIFY(images.addImage(source, QTransform().translate(10, 0)));
+    expectedBounds.adjust(0, 0, 10, 0);
+    QCOMPARE(images.boundingRect(), expectedBounds);
+
+    // Add a rotated image
+    QVERIFY(images.addImage(source, QTransform().rotate(45)));
+    expectedBounds =
+        expectedBounds.united(QTransform().rotate(45).mapRect(imageRect));
+    QCOMPARE(images.boundingRect(), expectedBounds);
 }
 
 QTEST_MAIN(AbcTest)
