e0ed9d7ee2968dc335711b38f2da834ca624c8e6
Alberto Mardegan <mardy@users.sourceforge.net>
2012-12-31 14:25:37 +0100

    Uploader, Controller: use FileLog class
    At startup, upload only the files that have been changed.
    Whenever an upload is completed, add it to the log.

--- uploader/controller.cpp
+++ uploader/controller.cpp
@@ -11,7 +11,9 @@
 #include "configuration.h"
 #include "controller.h"
 #include "debug.h"
+#include "file-log.h"
 #include "file-monitor.h"
+#include "upload-item.h"
 #include "upload-queue.h"
 
 #include <QDateTime>
@@ -29,12 +31,14 @@ class ControllerPrivate: public QObject
     ControllerPrivate(Controller *q);
 
 private Q_SLOTS:
-    void updateWatcher();
+    void onUploadPathChanged();
     void onDirectoryChanged();
+    void onDataChanged(const QModelIndex &first, const QModelIndex &last);
 
 private:
     QDateTime lastUpdateTime;
     FileMonitor watcher;
+    FileLog fileLog;
     mutable Controller *q_ptr;
 };
 
@@ -46,23 +50,32 @@ ControllerPrivate::ControllerPrivate(Controller *q):
 {
     Configuration *configuration =
        Application::instance()->configuration();
-    lastUpdateTime = configuration->lastUploadTime();
     QObject::connect(configuration, SIGNAL(uploadPathChanged()),
-                     this, SLOT(updateWatcher()));
+                     this, SLOT(onUploadPathChanged()));
 
     QObject::connect(&watcher, SIGNAL(changed()),
                      this, SLOT(onDirectoryChanged()));
     watcher.setBasePath(configuration->uploadPath());
 
+    fileLog.setBasePath(configuration->uploadPath());
+
+    /* Monitor completed uploads (and update the FileLog) */
+    UploadQueue *uploadQueue = Application::instance()->uploadQueue();
+    connect(uploadQueue,
+            SIGNAL(dataChanged(const QModelIndex &, const QModelIndex &)),
+            this,
+            SLOT(onDataChanged(const QModelIndex &, const QModelIndex &)));
+
     /* Force a refresh */
     onDirectoryChanged();
 }
 
-void ControllerPrivate::updateWatcher()
+void ControllerPrivate::onUploadPathChanged()
 {
     Configuration *configuration =
        Application::instance()->configuration();
     watcher.setBasePath(configuration->uploadPath());
+    fileLog.setBasePath(configuration->uploadPath());
 
     /* The last upload time needs to be reset, since this is a new
      * directory and we'd better check every file in it. */
@@ -77,16 +90,40 @@ void ControllerPrivate::onDirectoryChanged()
     QDateTime newLastUpdateTime = QDateTime::currentDateTime();
 
     QStringList allFiles = watcher.filesChangedSince(lastUpdateTime);
-    if (allFiles.isEmpty()) return;
+    /* Check that the files actually need an upload; this is especially
+     * needed when the object is first instantiated: since lastUpdateTime
+     * is invalid we get the list of all monitored files, so we check
+     * them against our upload log. */
+    QStringList changedFiles = fileLog.filterOutLogged(allFiles);
+    if (changedFiles.isEmpty()) return;
 
     UploadQueue *uploadQueue = Application::instance()->uploadQueue();
 
-    foreach (const QString &fileName, allFiles) {
+    foreach (const QString &fileName, changedFiles) {
         DEBUG() << "File:" << fileName;
         uploadQueue->requestUpload(fileName);
     }
 
     lastUpdateTime = newLastUpdateTime;
+}
+
+void ControllerPrivate::onDataChanged(const QModelIndex &first,
+                                      const QModelIndex &last)
+{
+    UploadQueue *uploadQueue = Application::instance()->uploadQueue();
+
+    for (QModelIndex i = first;
+         i < last;
+         i = i.sibling(i.row() + 1, i.column())) {
+        QVariant data = uploadQueue->data(i, UploadQueue::UploadItemRole);
+        UploadItem *item = data.value<UploadItem *>();
+        if (Q_UNLIKELY(!item)) continue;
+
+        if (item->progress() < 100) continue;
+
+        DEBUG() << "Upload completed:" << item->fileName();
+        fileLog.addFile(item->fileName());
+    }
 }
 
 Controller::Controller(QObject *parent):
