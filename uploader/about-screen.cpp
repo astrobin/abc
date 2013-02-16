4e53ed14c3929cb1488ea7d341b420e184290d03
Alberto Mardegan <mardy@users.sourceforge.net>
2013-02-16 13:40:18 +0400

    Uploader: add minimal About screen

--- uploader/about-screen.cpp
+++ uploader/about-screen.cpp
@@ -10,11 +10,40 @@
 #include "about-screen.h"
 #include "debug.h"
 
+#include <QLabel>
+#include <QPixmap>
+#include <QVBoxLayout>
+
 using namespace ABC;
 
 AboutScreen::AboutScreen(QWidget *parent):
     QDialog(parent)
 {
+    setWindowTitle(tr("Astrobin Uploader"));
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->setContentsMargins(64, 11, 64, 11);
+
+    QLabel *logo = new QLabel;
+    logo->setPixmap(QPixmap(":logo"));
+    logo->setAlignment(Qt::AlignHCenter);
+    layout->addWidget(logo);
+    layout->addStretch();
+
+    QLabel *name = new QLabel;
+    name->setText(tr("Astrobin Uploader"));
+    name->setAlignment(Qt::AlignHCenter);
+    name->setStyleSheet("QLabel { font: bold 16px; }");
+    layout->addWidget(name);
+
+    QLabel *version = new QLabel;
+    version->setText(tr("Version %1").arg(PROJECT_VERSION));
+    version->setAlignment(Qt::AlignHCenter);
+    version->setStyleSheet("QLabel { font: 12px; }");
+    layout->addWidget(version);
+    layout->addStretch();
+
+    setLayout(layout);
 }
 
 AboutScreen::~AboutScreen()
